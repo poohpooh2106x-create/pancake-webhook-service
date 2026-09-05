669e1ddc09875807984512142d8e168af2e72449
poohpooh2106x-create <poohpooh2106x-create@users.noreply.github.com>
2026-09-05 09:42:55 +0700

    Report modal: fix custom date picker not opening on tap
    Some mobile browsers/webviews don't reliably open a native
    <input type="date">'s picker on tap/click. Explicitly call
    showPicker() on click so it opens consistently.

--- api/webhooks/pancake.js
+++ api/webhooks/pancake.js
@@ -5,7 +5,7 @@
 const { google } = require('googleapis');
 const https = require('https');
 
-const APP_VERSION = '2026.09.05.2';
+const APP_VERSION = '2026.09.05.3';
 
 // ---------------------------------------------------------------------------
 // STORAGE LAYER
